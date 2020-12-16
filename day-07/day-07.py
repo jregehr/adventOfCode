3b4ff6717eeaa331df4c0354aeac2db252c3b862
jregehr <10343188+jregehr@users.noreply.github.com>
2020-12-16 16:01:29 -0600

    Finished Day 7

--- day-07/day-07.py
+++ day-07/day-07.py
@@ -3,7 +3,7 @@ import sys
 import csv
 
 if len(sys.argv) != 3:
-  sys.exit('Please pass the file name and the containing bag color as arguments.')
+    sys.exit('Please pass the file name and the containing bag color as arguments.')
 
 file = sys.argv[1]
 bagColor = sys.argv[2]
@@ -12,60 +12,104 @@ print(f'Loading {file}')
 
 # Read in the file
 with open(file) as f:
-  lines = [line.strip() for line in f]
+    lines = [line.strip() for line in f]
 
 
 # print(lines)
 print(f'Loaded {len(lines)} lines.')
 
-bagRelationships = {}
+containedBagRelationships = {}
 # contained bag is the key
 # containing bags array is the value
 
+containingBagRelationships = {}
+# containing bag is the key
+# contained bags array is the value
+
 
 def getBagColor(bag):
-  sbag = bag.split(' ')
-  return f'{sbag[1]} {sbag[2]}'
+    sbag = bag.split(' ')
+    return f'{sbag[1]} {sbag[2]}'
+
+
+def getNumberAndBagColor(bag):
+    sbag = bag.split(' ')
+    return [int(sbag[0]), f'{sbag[1]} {sbag[2]}']
 
 
 for bagLine in lines:
-  relship = bagLine.split(' bags contain ')
-  if relship[1] == 'no other bags.':
-    continue
+    relship = bagLine.split(' bags contain ')
+    if relship[1] == 'no other bags.':
+        continue
 
-  containedBags = relship[1].split(', ')
-  for containedBag in containedBags:
-    cBagCol = getBagColor(containedBag)
-    containedBagFitsIn = bagRelationships.get(cBagCol)
-    if containedBagFitsIn == None:
-      bagRelationships[cBagCol] = [relship[0]]
-    else:
-      bagRelationships[cBagCol].append(relship[0])
+    containedBags = relship[1].split(', ')
+    for containedBag in containedBags:
+        cBagCol = getBagColor(containedBag)
+        containedBagFitsIn = containedBagRelationships.get(cBagCol)
+        if containedBagFitsIn == None:
+            containedBagRelationships[cBagCol] = [relship[0]]
+        else:
+            containedBagRelationships[cBagCol].append(relship[0])
+        cNumBagCol = getNumberAndBagColor(containedBag)
+        bagContainedBy = containingBagRelationships.get(relship[0])
+        if bagContainedBy == None:
+            containingBagRelationships[relship[0]] = [cNumBagCol]
+        else:
+            containingBagRelationships[relship[0]].append(cNumBagCol)
 
-# for bagRel in bagRelationships.keys():
-#   print(f'{bagRel} goes in {bagRelationships[bagRel]}')
+print('=== contained bag relationships ==============')
+for bagRel in containedBagRelationships.keys():
+    print(f'{bagRel} goes in {containedBagRelationships[bagRel]}')
+
+print('=== containing bag relationships ==============')
+for bagRel in containingBagRelationships.keys():
+    print(f'{bagRel} has {containingBagRelationships[bagRel]}')
 
 containables = set()
+bagCount = 0
 
 
 def canContain(bagColor, containables):
   # print(f'entering canContain({bagColor})')
-  containers = bagRelationships.get(bagColor)
+  containers = containedBagRelationships.get(bagColor)
   if containers == None:
-    print('outermost')
-    # return f'\n{bagColor}'
-    containables.add(bagColor)
-    return containables
+      # print('outermost')
+      # return f'\n{bagColor}'
+      containables.add(bagColor)
+      return containables
 
   for containerColor in containers:
-    containables.add(containerColor)
-    # retVal += f'\n{containerColor}'
-    # retVal += f'{canContain(containerColor)} contains {bagColor} '
-    containables = canContain(containerColor, containables)
+      containables.add(containerColor)
+      # retVal += f'\n{containerColor}'
+      # retVal += f'{canContain(containerColor)} contains {bagColor} '
+      containables = canContain(containerColor, containables)
   return containables
 
 
+def bagContains(bagColor, bagCount):
+  print(f'entering bagContains({bagColor})')
+  containers = containingBagRelationships.get(bagColor)
+  if containers == None:
+    print(f'{bagColor} is empty')
+    return bagCount
+
+  for container in containers:
+    print(f'contains {container[0]} {container[1]}')
+    # bagCount += 1                                  #the bag we are in
+    bagCountA = container[0]                       # the bags we contain
+    bagCountB = bagContains(container[1], 1)
+    bagCount += (bagCountA * bagCountB)
+    # bagCount = bagContains(container[1], bagCount) # the bags they contain
+    
+  return bagCount
+
+
+print('\n\n=== results =========================')
+
+
 containables = canContain(bagColor, containables)
-print(len(containables))
+print(f'{len(containables)} bags can contain a {bagColor} bag.')
 print(containables)
-# canContain(bagColor)
+
+bagCount = bagContains(bagColor, bagCount)
+print(f'A {bagColor} bag contains {bagCount} bags')
